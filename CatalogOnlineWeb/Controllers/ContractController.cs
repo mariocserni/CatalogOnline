6ac3f5f76c9b2ff98ce4d42c84ecf65d3d91e135
Mario Cserni <r_marioroberto@yahoo.com>
2024-10-29 20:08:10 +0200

    Added AdaugaNote functionality

--- CatalogOnlineWeb/Controllers/ContractController.cs
+++ CatalogOnlineWeb/Controllers/ContractController.cs
@@ -47,5 +47,61 @@ namespace CatalogOnlineWeb.Controllers
             
             return View(SituatieScolara);
         }
+
+        public IActionResult AdaugaNote(int studentId)
+        {
+            var student = _db.Studenti.Find(studentId);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            List<Disciplina> discipline = _db.Discipline.Where(d => d.An <= student.An).ToList();
+            List<Contract> contracte = _db.Contracte.Where(c => c.StudentId == studentId).ToList();
+            var model = new SituatieScolaraViewModel(student, contracte, discipline);
+            return View(model);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult AdaugaNote(int disciplinaId, int studentId, double NotaParcurs, double NotaPrezentarea1, double NotaPrezentarea2, double NotaPrezentarea3)
+        {
+            var contract = _db.Contracte.FirstOrDefault(c => c.StudentId == studentId && c.DisciplinaId == disciplinaId);
+
+            if (contract != null)
+            {
+                contract.NotaParcurs = NotaParcurs;
+                contract.NotaPrezentarea1 = NotaPrezentarea1;
+                contract.NotaPrezentarea2 = NotaPrezentarea2;
+                contract.NotaPrezentarea3 = NotaPrezentarea3;
+
+                contract.Medie = (NotaParcurs + NotaPrezentarea1 + NotaPrezentarea2 + NotaPrezentarea3) / 4;
+
+                _db.SaveChanges(); 
+            }
+
+            return RedirectToAction("SituatieScolara", new { id = studentId });
+        }
+
+        [HttpGet]
+        public IActionResult GetNoteDisciplina(int studentId, int disciplinaId)
+        {
+            var contract = _db.Contracte.FirstOrDefault(c => c.StudentId == studentId && c.DisciplinaId == disciplinaId);
+            if (contract == null)
+            {
+                return Json(new { success = false });
+            }
+            return Json(new
+            {
+                success = true,
+                NotaParcurs = contract.NotaParcurs,
+                NotaPrezentarea1 = contract.NotaPrezentarea1,
+                NotaPrezentarea2 = contract.NotaPrezentarea2,
+                NotaPrezentarea3 = contract.NotaPrezentarea3
+            });
+        }
+
+
+
     }
 }
